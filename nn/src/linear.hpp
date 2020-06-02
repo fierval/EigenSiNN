e708c664116cfb0abead5ae5df8c7255d22298bf
fierval <fierval@gmail.com>
2020-06-01 20:07:15 -0700

    bias

--- nn/src/linear.hpp
+++ nn/src/linear.hpp
@@ -10,10 +10,13 @@ using namespace Eigen;
 class Linear : LayerBase {
 
 public:
-  Linear(int _in_dim, int _out_dim, bool _useBias = false)
-    : useBias(_useBias),
+  Linear(int _in_dim, int _out_dim, bool _use_bias = false)
+    : use_bias(_use_bias),
     in_dim(_in_dim),
-    out_dim(_out_dim) {}
+    out_dim(_out_dim) {
+      
+    biased_dim = use_bias ? in_dim + 1 : in_dim;
+    }
 
   virtual void forward(const MatrixXd& prev_layer_out) {
 
@@ -27,10 +30,13 @@ public:
     if (in_dim <= 0 || out_dim <= 0 || in_dim > MAX_ELEM || out_dim > MAX_ELEM) {
       throw std::invalid_argument("inappropriate dimensions");
     }
+
+    weights.resize(out_dim, biased_dim);
   }
 
 private:
   MatrixXd weights;
-  bool useBias;
+  bool use_bias;
   const int in_dim, out_dim;
+  int biased_dim;
 };
