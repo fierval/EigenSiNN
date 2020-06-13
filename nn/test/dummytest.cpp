e291ef3193b887d4e72632b1aead9e58358d340f
fierval <fierval@gmail.com>
2020-06-13 12:33:08 -0700

    gtest working

--- nn/test/dummytest.cpp
+++ nn/test/dummytest.cpp
@@ -4,61 +4,52 @@
 #include <iostream>
 
 #include "tensortest.hpp"
+#include <gtest/gtest.h>
 
-void test_fully_connected() {
-  
-  const int linear_layer_out = 3, batch_size = 2, linear_layer_in = 4;
-  MatrixXd input_matrix;
-  input_matrix.resize(batch_size, linear_layer_in);
-  input_matrix << 
-    1, 2, 3, 4,
-    5, 6, 7, 8;
+namespace eigen_tests {
 
-  //initialization constants
-  RNG rng(1);
-  double mu = 0;
-  double sigma = 0.01;
+    TEST(FullyConnected, BackpropTest) {
+        const int linear_layer_out = 3, batch_size = 2, linear_layer_in = 4;
+        MatrixXd input_matrix;
+        input_matrix.resize(batch_size, linear_layer_in);
+        input_matrix <<
+            1, 2, 3, 4,
+            5, 6, 7, 8;
 
-  InputLayer input(input_matrix);
-  Linear linear(input.batch_size(), input.input_vector_dim(), linear_layer_out);
+        //initialization constants
+        RNG rng(1);
+        double mu = 0;
+        double sigma = 0.01;
 
-  linear.init(rng, mu, sigma, true);
+        InputLayer input(input_matrix);
+        Linear linear(input.batch_size(), input.input_vector_dim(), linear_layer_out);
 
-  MatrixXd final_grad = MatrixXd::Ones(input.batch_size(), linear_layer_out);
-  linear.forward(input.get_layer());
-  linear.backward(input.get_layer(), final_grad);
+        linear.init(rng, mu, sigma, true);
 
-  // expected
-  MatrixXd Y, dLdX, dLdW;
-  Y << 10, 10, 10,
-      26, 26, 26;
+        MatrixXd final_grad = MatrixXd::Ones(input.batch_size(), linear_layer_out);
+        linear.forward(input.get_layer());
+        linear.backward(input.get_layer(), final_grad);
 
-  dLdX << 3, 3, 3, 3,
-      3, 3, 3, 3;
+        // expected
+        MatrixXd Y, dLdX, dLdW;
 
-  dLdW << 6, 6, 6,
-      8, 8, 8,
-      10, 10, 10,
-      12, 12, 12;
+        Y.resize(batch_size, linear_layer_out);
+        dLdX.resize(batch_size, linear_layer_in);
+        dLdW.resize(linear_layer_in, linear_layer_out);
 
-  std::cout << "X: " << std::endl;
-  std::cout << input.get_layer() << std::endl << std::endl;
+        Y << 10, 10, 10,
+            26, 26, 26;
 
-  std::cout << "Weights: " << std::endl;
-  std::cout << linear.get_weights() << std::endl << std::endl;
+        dLdX << 3, 3, 3, 3,
+            3, 3, 3, 3;
 
-  std::cout << "Y:" << std::endl;
-  std::cout << linear.get_output() << std::endl << std::endl;
+        dLdW << 6, 6, 6,
+            8, 8, 8,
+            10, 10, 10,
+            12, 12, 12;
 
-  std::cout << "dL/dX:" << std::endl;
-  std::cout << linear.get_loss_by_input_derivative() << std::endl << std::endl;
-
-  std::cout << "dL/dW:" << std::endl;
-  std::cout << linear.get_loss_by_weights_derivative() << std::endl << std::endl;
-}
-
-int main(int argc, char* argv[]) {
-
-  test_fully_connected();
-  return 0;
+        EXPECT_EQ(true, Y.isApprox(linear.get_output())) << "Failed: output test";
+        EXPECT_EQ(true, dLdX.isApprox(linear.get_loss_by_input_derivative())) << "Failed dL/dX";
+        EXPECT_EQ(true, dLdW.isApprox(linear.get_loss_by_weights_derivative())) << "Failed dL/dW";
+    }
 }
