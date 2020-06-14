876722b620ed12f08aa27c5bda62bac0db843b1c
fierval <fierval@gmail.com>
2020-06-14 10:58:45 -0700

    batched convolutions

--- nn/test/convolve_test.cpp
+++ nn/test/convolve_test.cpp
@@ -7,35 +7,35 @@
 #include <iostream>
 #include <utility>
 
-typedef Eigen::Tensor<float, 3> ConvTensor;
-typedef Eigen::Tensor<float, 4> ConvKernel;
+typedef Eigen::Tensor<float, 4> ConvTensor;
 
 // NWHC format
-ConvTensor convolve(ConvTensor& input, ConvKernel& kernel) {
+ConvTensor convolve(ConvTensor& input, ConvTensor& kernel) {
 
-    Eigen::array<Eigen::Index, 3> dims({0, 1, 2});
+    Eigen::array<Eigen::Index, 3> dims({1, 2, 3});
 
-    assert(input.dimension(2) == kernel.dimension(3));
+    assert(input.dimension(3) == kernel.dimension(3));
 
-    ConvTensor output(input.dimension(0) - kernel.dimension(1) + 1, input.dimension(1) - kernel.dimension(2) + 1, kernel.dimension(0));
+    ConvTensor output(input.dimension(0), input.dimension(1) - kernel.dimension(1) + 1, input.dimension(2) - kernel.dimension(2) + 1, kernel.dimension(0));
 
     for(int i = 0; i < kernel.dimension(0); i++) {
 
         // final chip(0, 0) removes the third dimension
-        output.chip(i, 2) = input.convolve(kernel.chip(i, 0), dims).chip(0, 2);
+        output.chip(i, 3) = input.convolve(kernel.chip(i, 0), dims).chip(0, 3);
     }
 
     return output;
 }
 
-ConvTensor convolve_full(ConvTensor& input, ConvKernel& kernel) {
+ConvTensor convolve_full(ConvTensor& input, ConvTensor& kernel) {
     int dim1 = kernel.dimension(1) - 1;
     int dim2 = kernel.dimension(2) - 1;
 
-    Eigen::array<std::pair<int, int>, 3> paddings;
-    paddings[0] = std::make_pair(dim1, dim1);
-    paddings[1] = std::make_pair(dim2, dim2);
-    paddings[2] = std::make_pair(0, 0);
+    Eigen::array<std::pair<int, int>, 4> paddings;
+    paddings[0] = std::make_pair(0, 0);
+    paddings[1] = std::make_pair(dim1, dim1);
+    paddings[2] = std::make_pair(dim2, dim2);
+    paddings[3] = std::make_pair(0, 0);
 
     ConvTensor padded = input.pad(paddings);
 
@@ -44,29 +44,29 @@ ConvTensor convolve_full(ConvTensor& input, ConvKernel& kernel) {
 }
 
 TEST(Convolution, SingleStride) {
-    ConvTensor input(4, 4, 2);
-    ConvKernel kernel(3, 3, 3, 2);
+    ConvTensor input(5, 4, 4, 2);
+    ConvTensor kernel(3, 3, 3, 2);
 
     input.setConstant(3);
     kernel.setConstant(1);
 
     ConvTensor output = convolve(input, kernel);
-    EXPECT_EQ(output(0,0,0), 3 * 9 * 2) << "Failed value test";
-    EXPECT_EQ(output.dimension(0), 2) << "Failed dim[0] test";
+    EXPECT_EQ(output(0,0,0, 0), 3 * 9 * 2) << "Failed value test";
     EXPECT_EQ(output.dimension(1), 2) << "Failed dim[1] test";
-    EXPECT_EQ(output.dimension(2), 3) << "Failed dim[2] test";
+    EXPECT_EQ(output.dimension(2), 2) << "Failed dim[2] test";
+    EXPECT_EQ(output.dimension(3), 3) << "Failed dim[3] test";
 }
 
 TEST(Convolution, Full) {
-    ConvTensor input(4, 4, 2);
-    ConvKernel kernel(3, 3, 3, 2);
+    ConvTensor input(5, 4, 4, 2);
+    ConvTensor kernel(3, 3, 3, 2);
 
     input.setConstant(3);
     kernel.setConstant(1);
 
     ConvTensor output = convolve_full(input, kernel);
-    EXPECT_EQ(output(0,0,0), 3 * 2) << "Failed value test";
-    EXPECT_EQ(output.dimension(0), 6) << "Failed dim[0] test";
+    EXPECT_EQ(output(0,0,0, 0), 3 * 2) << "Failed value test";
     EXPECT_EQ(output.dimension(1), 6) << "Failed dim[1] test";
-    EXPECT_EQ(output.dimension(2), 3) << "Failed dim[2] test";
+    EXPECT_EQ(output.dimension(2), 6) << "Failed dim[2] test";
+    EXPECT_EQ(output.dimension(3), 3) << "Failed dim[3] test";
 }
