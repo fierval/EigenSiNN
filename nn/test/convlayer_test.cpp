14c4dbc4b55d3bac1210c87ab9f3006cc0806d40
fierval <fierval@gmail.com>
2020-06-20 18:43:08 -0700

    reverse test

--- nn/test/convlayer_test.cpp
+++ nn/test/convlayer_test.cpp
@@ -13,9 +13,9 @@ namespace EigenSinnTest {
     a.setValues({ {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12} });
 
     Eigen::array<bool, 2> rev_index({ true, false });
-    a.reverse(rev_index);
+    Eigen::Tensor<int, 2> b = a.reverse(rev_index);
 
-    std::cerr << a;
+    EXPECT_EQ(b(0, 0), 10);
   }
 
 }
