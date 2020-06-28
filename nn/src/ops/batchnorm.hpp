3da55ee831ebae5082a306340e48f90a4c3df4f2
fierval <fierval@gmail.com>
2020-06-27 22:38:11 -0700

    complete forward pass through batch norm

--- nn/src/ops/batchnorm.hpp
+++ nn/src/ops/batchnorm.hpp
@@ -10,21 +10,19 @@ namespace EigenSinn {
   typedef Eigen::Tensor<float, 1> ConvTensorSingle;
 
   // broadcast channel dimension - first in the list of arguments, las
-  template<typename... Dims>
-  inline ConvTensor broadcast_as_last_dim(ConvTensorSingle& t, Dims... dims) {
-    constexpr int rank = sizeof... (Dims);
+  template<Index rank>
+  inline ConvTensor broadcast_as_last_dim(ConvTensorSingle& t, Eigen::array<int, rank> broadcast_dims) {
 
-    Eigen::array<Index, rank + 1> reshaped_dims;
+    Eigen::array<Index, rank> reshaped_dims;
 
     reshaped_dims.fill(1);
     reshaped_dims[rank] = t.dimension(0);
-
-    Eigen::array<Index, rank + 1> broadcast_dims{ dims..., 1 };
 
     ConvTensor broadcasted = t.reshape(reshaped_dims).broadcast(broadcast_dims);
     return broadcasted;
   }
 
+  // NHWC format
   template<typename Scalar, Index Dim>
   inline auto batch_norm(const Eigen::Tensor<Scalar, Dim>& x, float gamma, float beta, float eps, float momentum, 
     ConvTensorSingle& running_mean, ConvTensorSingle running_var, bool isTraining) {
@@ -33,22 +31,32 @@ namespace EigenSinn {
     ConvTensor mu_broadcasted, running_mean_broadcasted, running_var_broadcasted, x_hat, std_broadcasted;
 
     // get sample mean
-    Eigen::array<int, 3> reduction_dims({ 0, 1, 2 });
-    
+    Eigen::array<int, Dim - 1> reduction_dims;
+    Eigen::array<int, Dim> broadcast_dims;
+
+    // we reduce by all dimensions but the last (channel)
+    // and broadcast all but the last
+    for (int i = 0; i < Dim - 1; i++) {
+      reduction_dims[i] = i;
+      broadcast_dims[i] = x.dimension(i);
+    }
+
+    broadcast_dims[Dim - 1] = 1;
+
     mu = x.mean(reduction_dims);
     Index batch_size = x.dimension(0);
     Index n_channels = x.dimension(3);
     Index rows = x.dimension(1);
     Index cols = x.dimension(2);
 
-    mu_broadcasted = broadcast_as_last_dim(mu, batch_size, rows, cols);
+    mu_broadcasted = broadcast_as_last_dim(mu, broadcast_dims);
     variance = (x - mu_broadcasted).pow(2.).mean(reduction_dims);
 
     running_mean = momentum * running_mean + (1.0 - momentum) * mu;
     running_var = momentum * running_var + (1.0 - momentum) * variance;
 
-    running_mean_broadcasted = broadcast_as_last_dim(running_mean, batch_size, rows, cols);
-    running_var_broadcasted = broadcast_as_last_dim(running_var, batch_size, rows, cols);
+    running_mean_broadcasted = broadcast_as_last_dim(running_mean, broadcast_dims);
+    running_var_broadcasted = broadcast_as_last_dim(running_var, broadcast_dims);
     std_broadcasted = (running_var_broadcasted + eps).sqrt();
 
 
