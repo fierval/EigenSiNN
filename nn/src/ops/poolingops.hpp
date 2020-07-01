7cb2c649e3909427436c9bbb29f111a2fcbce0e3
fierval <fierval@gmail.com>
2020-06-30 23:34:43 -0700

    maxpool 2d

--- nn/src/ops/poolingops.hpp
+++ nn/src/ops/poolingops.hpp
@@ -7,13 +7,13 @@
 
 namespace EigenSinn {
 
-  enum PoolType : int{
+  enum PoolType : int {
     max,
     avg
   };
 
   template <int Rank, class Dims>
-  inline bool check_valid_params(const array<int, Rank/2>& extents, int stride, Dims& dims) {
+  inline bool check_valid_params(const array<int, Rank / 2>& extents, int stride, Dims& dims) {
 
     if (stride <= 0) {
       return false;
@@ -24,7 +24,7 @@ namespace EigenSinn {
       return false;
     }
 
-    for (int i = 0; i < Rank/2; i++) {
+    for (int i = 0; i < Rank / 2; i++) {
       // we are interested in the second or second and third dimensions
       // depending on the tensor: 2d or 4d
       int tensor_dim = dims[i + 1];
@@ -42,19 +42,41 @@ namespace EigenSinn {
     return true;
   }
 
-  
 
-  template <int Rank, int Dim>
-  inline auto do_pool(NnTensor<Rank>& t, const array<int, Dim>& extents, int stride) {
-    
+
+  template <int Rank>
+  inline NnTensor<Rank> do_pool(NnTensor<Rank>& t, const array<int, Rank / 2>& extents, int stride) {
+
+    return NnTensor<Rank>();
+  }
+
+  inline NnTensor<2> do_pool(NnTensor<2>& t, const array<int, 1>& extents, int stride) {
     auto dims = t.dimensions();
-    
-    if (!check_valid_params(extens, stirde, dims)) {
+
+    if (!check_valid_params<2>(extents, stride, dims)) {
 
       throw std::invalid_argument("Invalid pooling dimensions");
     }
 
+    NnTensor<2> output(dims[0], (dims[1] - extents[0]) / stride + 1);
 
+    array<Index, 2> starts({ 0, 0 });
+    array<Index, 2> lengths({ dims[0], extents[0] });
+    array<Index, 1> reduce_dims({ 1 });
+
+    array<Index, 2> output_starts({ 0, 0 });
+    array<Index, 2> output_lengths({ output.dimension(0), 1 });
+
+    for (int i = 0; i + extents[0] < -dims[1]; i++) {
+      NnTensor<2> sl(dims[0], 1);
+      sl = t.slice(starts, lengths);
+      output.slice(output_starts, output_lengths) = sl.reduce(reduce_dims, internal::MaxReducer<float>());
+      output_starts[1]++;
+      starts[1] += extents[0];
+    }
+
+    return output;
 
   }
+
 }
