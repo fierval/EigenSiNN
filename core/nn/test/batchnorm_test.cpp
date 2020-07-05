5ff4bb7f905ca43dc18b874b14d733d2de3a6d95
fierval <fierval@gmail.com>
2020-07-04 17:49:10 -0700

    use test fixture

--- core/nn/test/batchnorm_test.cpp
+++ core/nn/test/batchnorm_test.cpp
@@ -8,17 +8,24 @@ using namespace EigenSinn;
 
 namespace EigenSinnTest {
 
-  TEST(Batchnorm, Forward1d) {
+  class BatchnormTest : public ::testing::Test {
+  protected:
+    void SetUp() override {
 
+      MatrixXf input_matrix;
+      input_matrix.resize(2, 5);
+
+      input_matrix << 0.5628, 0.9343, 0.2593, 0.7921, 0.1589,
+        0.1851, 0.0431, 0.5097, 0.8821, 0.5831;
+
+      LinearTensor input = Matrix_to_Tensor(input_matrix, 2, 5);
+    }
+
+    LinearTensor input;
     const float eps = 1e-5, momentum = 0.9;
+  };
 
-    MatrixXf input_matrix;
-    input_matrix.resize(2, 5);
-
-    input_matrix << 0.5628, 0.9343, 0.2593, 0.7921, 0.1589,
-      0.1851, 0.0431, 0.5097, 0.8821, 0.5831;
-
-    LinearTensor input = Matrix_to_Tensor(input_matrix, 2, 5);
+  TEST_F(BatchnormTest, Forward1d) {
 
     BatchNormalizationLayer bn(eps, momentum);
 
