8ed87cabd850a5a062fe50ffb24885813ae2b997
fierval <fierval@gmail.com>
2020-07-04 19:34:17 -0700

    batchnorm data generation test

--- python/testgen/batchhorm.py
+++ python/testgen/batchhorm.py
@@ -1,15 +1,18 @@
 import torch
-import numpy as np
 import torch.nn as nn
 
 device = torch.device("cuda")
 
 
-input = torch.tensor([[0.5628, 0.9343, 0.2593, 0.7921, 0.1589],
+inp = torch.tensor([[0.5628, 0.9343, 0.2593, 0.7921, 0.1589],
         [0.1851, 0.0431, 0.5097, 0.8821, 0.5831]], device='cuda:0',
        requires_grad=True)
 
 bn = nn.BatchNorm1d(5, eps=1e-5, momentum=0.1).cuda()
+bn.weight.data = \
+    torch.tensor([0, 1, 2, 3, 4], dtype=float, device=device)
+
+bn.bias.data = bn.weight.data / 10.
 o = torch.ones((2, 5), device=device, dtype=float)
 
-output = bn(input)
+output = bn(inp)
