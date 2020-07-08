3a272294122dac3f9e2efa55dd1b6e9d5af9900b
fierval <fierval@gmail.com>
2020-07-07 19:07:15 -0700

    prep backprop bn

--- python/testgen/batchhorm.py
+++ python/testgen/batchhorm.py
@@ -6,14 +6,16 @@ device = torch.device("cuda")
 
 inp = torch.tensor([[0.5628, 0.9343, 0.2593, 0.7921, 0.1589],
         [0.1851, 0.0431, 0.5097, 0.8821, 0.5831]], device='cuda:0', dtype=torch.float,
-       requires_grad=True)
+       requires_grad=True) 
 
 bn = nn.BatchNorm1d(5, eps=1e-5, momentum=0.1).cuda()
 bn.weight.data = \
     torch.tensor([1., 2., 3., 4., 5.], dtype=torch.float, device=device)
 
-bn.bias.data = bn.weight.data / 10.
-o = torch.ones((2, 5), device=device, dtype=torch.float)
+bn.bias.data = \
+    torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5], dtype=torch.float, device=device)
+
+o = torch.rand((2, 5), device=device, dtype=torch.float)
 
 output = bn(inp)
 output.backward(o)
