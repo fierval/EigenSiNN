54dc602cedd0e55ac54a1d664b7c97538649efc9
fierval <fierval@gmail.com>
2020-07-13 22:11:50 -0700

    data gen

--- python/testgen/relu.py
+++ python/testgen/relu.py
@@ -2,9 +2,16 @@ import commondata2d as cd
 import torch
 import torch.nn as nn
 
+############# ReLU ############################
 inp = cd.inp
 fakeloss = cd.fakeloss
 
 rl = nn.ReLU()
 output = rl(inp)
+output.backward(fakeloss.reshape_as(output))
+
+############# Leaky ReLU #####################
+inp.grad.zero_()
+lrl = nn.LeakyReLU(negative_slope=1e-2)
+output = lrl(inp)
 output.backward(fakeloss.reshape_as(output))
