e34a8fbcfe007a00e2d2c7d9a7cd814777ce6e3b
fierval <fierval@gmail.com>
2020-07-27 17:14:34 -0700

    mse experiments

--- python/losses/mse.py
+++ python/losses/mse.py
@@ -2,6 +2,36 @@ import tstcommon.commondata2d as cd
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import numpy as np
 
 inp = cd.inp.reshape(cd.inp.shape[1:])
+inp.requires_grad_()
 
+in_feat = 8
+out_feat = 4
+batch_size = 3
+
+fc = nn.Linear(in_feat, out_feat, bias=False)
+
+fc.weight.data = cd.weights
+optimizer = optim.SGD(fc.parameters(), lr=0.01, momentum=0.9)
+loss_fn = nn.MSELoss()
+
+output = fc(inp)
+output.requires_grad_()
+output.retain_grad()
+
+loss = loss_fn(output, cd.target)
+loss.retain_grad()
+
+loss.backward()
+optimizer.step()
+
+##################################################
+
+loss_act = ((output - cd.target) ** 2).mean()
+
+dav = 1./ (batch_size * out_feat) *torch.from_numpy(np.ones((batch_size, out_feat)))
+
+dsq = 2 * dav * (output - cd.target)
