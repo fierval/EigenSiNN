fix softmax test data

python/layers/softmax.py
@@ -14,8 +14,10 @@ import torch.nn as nn
 inp = cd.inp
 fakeloss = cd.fakeloss
 
-sg = nn.Softmax(dim=1)
+sg = nn.Softmax(dim=2)
 output = sg(inp)
 output.backward(fakeloss.reshape_as(output))
 
+print(f"output: {to_cpp(output)}")
+print(f"dinput: {to_cpp(inp.grad)}")
 # %%