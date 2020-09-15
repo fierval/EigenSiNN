987bb9bacfc080c9d23e6a836125d813854c7e97
fierval <fierval@gmail.com>
2020-09-14 18:41:21 -0700

    dropout tests

--- core/nn/test/dropout_test.cpp
+++ core/nn/test/dropout_test.cpp
@@ -28,7 +28,39 @@ namespace EigenSinnTest {
 
     Tensor<float, 2> zeros(output.dimensions());
     zeros.setZero();
+    
     Tensor<bool, 0> anyzeros = (output == zeros).any();
+    Tensor<bool, 0> allzeros = (output == zeros).all();
     EXPECT_TRUE(anyzeros(0));
+    EXPECT_FALSE(allzeros(0));
+
+    for (Index i = 0; i < output.dimension(0); i++) {
+      for (Index j = 0; j < output.dimension(1); j++) {
+        EXPECT_TRUE(output(i, j) == 0. || output(i, j) == 2. * cd.linearInput(i, j));
+      }
+    }
   }
+
+  TEST_F(Dropout, Backward) {
+
+    EigenSinn::Dropout<float, 2> dropout;
+    dropout.forward(cd.linearInput);
+    dropout.backward(cd.linearInput, cd.linearLoss);
+
+    auto dinput = from_any<float, 2>(dropout.get_loss_by_input_derivative());
+
+    Tensor<float, 2> zeros(dinput.dimensions());
+    zeros.setZero();
+    Tensor<bool, 0> anyzeros = (dinput == zeros).any();
+    Tensor<bool, 0> allzeros = (dinput == zeros).all();
+    EXPECT_TRUE(anyzeros(0));
+    EXPECT_FALSE(allzeros(0));
+
+    for (Index i = 0; i < dinput.dimension(0); i++) {
+      for (Index j = 0; j < dinput.dimension(1); j++) {
+        EXPECT_TRUE(dinput(i, j) == 0. || dinput(i, j) == 2. * cd.linearLoss(i, j));
+      }
+    }
+  }
+
 }
