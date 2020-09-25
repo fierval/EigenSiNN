5d423fe4785793adb9762812d360fd980c74909c
fierval <fierval@gmail.com>
2020-09-24 21:55:45 -0700

    dimensionality funcs

--- core/nn/src/layers/layer_base.hpp
+++ core/nn/src/layers/layer_base.hpp
@@ -16,6 +16,8 @@ namespace EigenSinn {
 
     virtual void forward(std::any prev_layer_any) = 0;
 
+    virtual void forward(void* prev_layer) {};
+
     virtual void backward(std::any prev_layer, std::any next_layer_grad) = 0;
 
     virtual  std::any get_weights() { return std::any(); }
@@ -38,10 +40,17 @@ namespace EigenSinn {
 
     inline static Dispatcher<DefaultDevice> default_dispatcher = Dispatcher<DefaultDevice>();
 
+    const std::vector& get_in_dims() { return in_dims; }
+    const std::vector& get_out_dims() { return out_dims; }
+
+    void set_in_dims(std::vector<int>& _in_dims) { in_dims = _in_dims; }
+    void set_out_dims(std::vector<int>& _out_dims) { out_dims = _out_dims; }
+
   protected:
     Dispatcher<Device_>& dispatcher;
+    std::vector<int> in_dims, out_dims;
 
-    LayerBase(Dispatcher<Device_>& _dispatcher) : dispatcher(_dispatcher) { 
+    LayerBase(Dispatcher<Device_>& _dispatcher) : dispatcher(_dispatcher) {
     }
 
   };
