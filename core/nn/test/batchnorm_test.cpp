e15913a656d43f1e7c56d496d05cb6484954411b
fierval <fierval@gmail.com>
2020-11-16 18:57:56 -0800

    add forward check

--- core/nn/test/batchnorm_test.cpp
+++ core/nn/test/batchnorm_test.cpp
@@ -28,13 +28,17 @@ namespace EigenSinnTest {
       gamma.resize(cols);
       beta.resize(cols);
 
+      output.resize(input.dimensions());
+      output.setValues({ { 1.09985983, 2.19994974, -2.69904375, -3.59016252, -4.49944401},
+        {-0.89985991, -1.79994965, 3.29904366, 4.39015722, 5.49944496} });
+
       gamma.setValues({1., 2.0, 3., 4., 5.});
       beta = gamma * 0.1f;
     }
 
     //void TearDown() override {}
 
-    Tensor<float, 2> input, loss;
+    Tensor<float, 2> input, loss, output;
     TensorSingleDim<float> beta, gamma;
     const float eps = 1e-5, momentum = 0.9;
     const Index batch_size = 2, cols = 5;
@@ -58,8 +62,10 @@ namespace EigenSinnTest {
 
     bn.init(beta, gamma);
     bn.forward(input_layer);
-    bn.backward(input_layer, loss.data());
 
+    EXPECT_TRUE(is_elementwise_approx_eq(output, bn.get_output()));
+
+    bn.backward(input_layer, loss.data());
 
     EXPECT_TRUE(is_elementwise_approx_eq(expected_derivative, bn.get_loss_by_input_derivative(), 4e-5));
     EXPECT_TRUE(is_elementwise_approx_eq(exp_dbeta, bn.get_loss_by_bias_derivative(), 1e-5));
