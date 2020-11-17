3c3dfa503334bda9216bf68a335d502d2845e7c0
fierval <fierval@gmail.com>
2020-11-17 12:25:54 -0800

    start layer base transformation

--- core/nn/src/layers/layer_base.hpp
+++ core/nn/src/layers/layer_base.hpp
@@ -15,7 +15,7 @@ namespace EigenSinn {
     Bias = 2
   };
 
-  template <typename Scalar>
+  template <typename Device_, typename Scalar, Index InRank, Index OutRank, int Layout=ColMajor>
   class LayerBase {
 
   public:
@@ -28,28 +28,30 @@ namespace EigenSinn {
 
     virtual void backward(LayerBase<Scalar>& prev_layer, Scalar * next_layer_grad) = 0;
 
-    virtual  Scalar* get_weights() { return nullptr; }
+    virtual  DeviceTensor<Device_, Scalar, OutRank, Layout>& get_output() {
+      return layer_output;
+    }
 
-    virtual  Scalar* get_bias() { return nullptr; }
+    virtual  DeviceTensor<Device_, Scalar, InRank, Layout>& get_loss_by_input_derivative() {
+      return layer_gradient;
+    }
 
-    virtual  Scalar* get_output() = 0;
+    virtual  DeviceTensor<Device_, Scalar, InRank, Layout>& get_loss_by_weights_derivative() { return DeviceTensor(); };
+    virtual  DeviceTensor<Device_, Scalar, InRank, Layout>& get_weights() { return DeviceTensor(); };
 
-    virtual  Scalar* get_loss_by_input_derivative() = 0;
+    virtual  DeviceTensor<Device_, Scalar, 1, Layout>& get_loss_by_bias_derivative() { return DeviceTensor(); }
+    virtual  DeviceTensor<Device_, Scalar, 1, Layout>& get_bias() { return DeviceTensor(); }
 
-    virtual  Scalar* get_loss_by_weights_derivative() { return nullptr; };
+    DSizes<Index, InRank>& get_in_dims() { return in_dims; }
+    DSizes<Index, OutRank>& get_out_dims() { return out_dims; }
 
-    virtual  Scalar* get_loss_by_bias_derivative() { return nullptr; }
+    DSizes<Index, 1>& get_bias_dims() { return bias_dims; }
+    DSizes<Index, InRank>& get_weight_dims() { return weight_dims; }
 
-    std::vector<Index>& get_in_dims() { return in_dims; }
-    std::vector<Index>& get_out_dims() { return out_dims; }
+    void set_in_dims(DSizes<Index, InRank>& _in_dims) { in_dims = _in_dims; }
+    void set_out_dims(DSizes<Index, OutRank>& _out_dims) { out_dims = _out_dims; }
 
-    std::vector<Index>& get_bias_dims() { return bias_dims; }
-    std::vector<Index>& get_weight_dims() { return weight_dims; }
-
-    void set_in_dims(std::vector<Index>& _in_dims) { in_dims = _in_dims; }
-    void set_out_dims(std::vector<Index>& _out_dims) { out_dims = _out_dims; }
-
-    void set_dims(std::vector<Index>& _in_dims, std::vector<Index>& _out_dims) {
+    void set_dims(DSizes<Index, InRank>& _in_dims, DSizes<Index, OutRank>& _out_dims) {
       set_in_dims(_in_dims);
       set_out_dims(_out_dims);
     }
@@ -60,13 +62,20 @@ namespace EigenSinn {
       }
     }
 
-    void set_bias_dims(std::vector<Index>& _bias_dims) { bias_dims = _bias_dims; }
-    void set_weight_dims(std::vector<Index>& _weight_dims) { weight_dims = _weight_dims; }
+    void set_bias_dims(DSizes<Index, 1>& _bias_dims) { bias_dims = _bias_dims; }
+    void set_weight_dims(DSizes<Index, InRank>& _weight_dims) { weight_dims = _weight_dims; }
 
     virtual ~LayerBase() = default;
 
   protected:
-    std::vector<Index> in_dims, out_dims, bias_dims, weight_dims;
+    DSizes<Index, InRank> in_dims, weight_dims;
+    DSizes<Index, OutRank> out_dims; 
+    DSizes<Index, 1> bias_dims;
+
+    DeviceTensor<Device_, Scalar, OutRank, Layout> layer_output;
+    DeviceTensor<Device_, Scalar, InRank, Layout> layer_gradient;
+    DeviceTensor<Device_, Scalar, InRank, Layout> weights;
+    DeviceTensor<Device_, Scalar, 1, Layout> bias;
 
     bool are_dims_unset(std::vector<Index>& dims) { return in_dims.size() == 0 || dims[0] != in_dims[0]; }
 
