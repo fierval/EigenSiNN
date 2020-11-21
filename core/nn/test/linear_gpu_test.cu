5360b109f562099dad7e5ea2d234ee7b1e767817
fierval <fierval@gmail.com>
2020-11-21 15:53:23 -0800

    linear gpu tests

--- core/nn/test/linear_gpu_test.cu
+++ core/nn/test/linear_gpu_test.cu
@@ -4,14 +4,13 @@
 #include "include/testutils.hpp"
 #include "layers/input.hpp"
 #include "ops/comparisons.hpp"
-#include "device/device_helpers.hpp"
-#include "ops/conversions.hpp"
+#include "device/device_tensor.hpp"
 
 using namespace Eigen;
 using namespace EigenSinn;
 
 namespace EigenSinnTest {
-  class LinearGpu : public ::testing::Test {
+  class FullyConnectedGpu : public ::testing::Test {
 
   protected:
     void SetUp() override {
@@ -22,50 +21,56 @@ namespace EigenSinnTest {
     CommonData2d cd;
   };
 
-  TEST_F(LinearGpu, Initialization) {
+  TEST_F(FullyConnectedGpu, BackpropNoBias) {
 
-    Dispatcher<GpuDevice> dispatcher;
-    GpuDevice& device = dispatcher.get_device();
+    Input<float, 2, GpuDevice> input;
+    input.set_input(cd.linearInput);
 
-    array<Index, 2> kdims = {512, 3};
-    Linear<float, GpuDevice> linear(kdims[0], kdims[1]);
+    Linear<float, ColMajor, GpuDevice> linear(cd.dims[1], cd.out_dims[1]);
 
-    linear.init();
-    TensorMap<Tensor<float, 2>> weights(linear.get_weights(), vector2array<2>(linear.get_weight_dims()));
+    linear.init(cd.weights);
+    linear.forward(input);
+    linear.backward(input, DeviceTensor<GpuDevice, float, 2>(cd.fakeloss));
 
-    Tensor<float, 0> avg;
-    Tensor<float, 0> var;
-    Tensor<float, 0> var_expected;
-
-    var.device(device) = (weights - *(avg.data())).pow(2.).mean();
-    avg.device(device) = weights.mean();
-    var_expected.setConstant(1. / kdims[0]);
-
-    Tensor<float, 0> var_cpu = from_device(var.data());
-    EXPECT_TRUE(is_elementwise_approx_eq(var_expected, var, 1e-4));
+    EXPECT_TRUE((is_elementwise_approx_eq<float, 2, 0, GpuDevice>(cd.output, linear.get_output())));
+    EXPECT_TRUE((is_elementwise_approx_eq<float, 2, 0, GpuDevice>(cd.dinput, linear.get_loss_by_input_derivative())));
+    EXPECT_TRUE((is_elementwise_approx_eq<float, 2, 0, GpuDevice>(cd.dweights, linear.get_loss_by_weights_derivative())));
   }
 
-  //TEST_F(LinearGpu, Backward) {
-  //  Dispatcher<GpuDevice> device;
-  //  float* d_input = to_device(cd.linearInput);
+  TEST_F(FullyConnectedGpu, BackpropBias) {
 
-  //  Input<float, 2, GpuDevice> input(cd.dims, device);
-  //  input.set_input(d_input);
+    Input<float, 2, GpuDevice> input;
+    input.set_input(cd.linearInput);
 
-  //  Linear<float, GpuDevice> linear(cd.dims[1], cd.out_dims[1]);
+    Linear<float, ColMajor, GpuDevice> linear(cd.dims[1], cd.out_dims[1]);
 
-  //  linear.init(cd.weights, cd.bias);
-  //  linear.forward(input);
-  //  linear.backward(input, cd.fakeloss.data());
+    linear.init(cd.weights, cd.bias);
+    linear.forward(input);
+    linear.backward(input, DeviceTensor<GpuDevice, float, 2>(cd.fakeloss));
 
-  //  cd.output.setValues({ { 0.23947978,  1.57379603,  0.23219500,  0.99900943},
-  //      { 1.11431766, -1.17991734, -0.41367567,  1.14438200},
-  //      { 0.11173135,  0.14724597,  0.10992362,  1.70647931} });
+    cd.output.setValues({ { 0.23947978,  1.57379603,  0.23219500,  0.99900943},
+        { 1.11431766, -1.17991734, -0.41367567,  1.14438200},
+        { 0.11173135,  0.14724597,  0.10992362,  1.70647931} });
 
-  //  EXPECT_TRUE(is_elementwise_approx_eq(cd.output, linear.get_output()));
-  //  EXPECT_TRUE(is_elementwise_approx_eq(cd.dinput, linear.get_loss_by_input_derivative()));
-  //  EXPECT_TRUE(is_elementwise_approx_eq(cd.dweights, linear.get_loss_by_weights_derivative()));
-  //  EXPECT_TRUE(is_elementwise_approx_eq(cd.dbias, linear.get_loss_by_bias_derivative()));
+    EXPECT_TRUE((is_elementwise_approx_eq<float, 2, 0, GpuDevice>(cd.output, linear.get_output())));
+    EXPECT_TRUE((is_elementwise_approx_eq<float, 2, 0, GpuDevice>(cd.dinput, linear.get_loss_by_input_derivative())));
+    EXPECT_TRUE((is_elementwise_approx_eq<float, 2, 0, GpuDevice>(cd.dweights, linear.get_loss_by_weights_derivative())));
+    EXPECT_TRUE((is_elementwise_approx_eq<float, 1, 0, GpuDevice>(cd.dbias, linear.get_loss_by_bias_derivative())));
+  }
 
-  //}
+  TEST_F(FullyConnectedGpu, Initialize) {
+
+    int in_dim = 1024;
+    Linear<float, ColMajor, GpuDevice> linear(in_dim, 512);
+
+    linear.init();
+    Tensor<float, 2> weights(DeviceTensor<GpuDevice, float, 2>((linear.get_weights())).to_host());
+    Tensor<float, 0> avg = weights.mean();
+    Tensor<float, 0> std = (weights - avg(0)).pow(2.).mean();
+
+    Tensor<float, 0> std_expected;
+    std_expected.setConstant(1. / in_dim);
+
+    EXPECT_TRUE(is_elementwise_approx_eq(std_expected, std, 1e-4));
+  }
 }
