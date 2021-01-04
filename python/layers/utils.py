bf984665b97582ea6f2dc35be597be7718ae4dd2
fierval <fierval@gmail.com>
2021-01-04 00:37:01 -0800

    tweak regex for to_cpp

--- python/layers/utils.py
+++ python/layers/utils.py
@@ -5,7 +5,7 @@ def to_cpp(x):
     s = str(x).replace("[", "{").replace("]", "}").replace("tensor", "")
 
     # remove any other tensor parameters at the end
-    regex = r"\,\s+[A-z_]+.+\)$"
+    regex = r"\,\s+[A-z]+.+\)$"
     s = re.sub(regex, ')', s)
 
     return s
