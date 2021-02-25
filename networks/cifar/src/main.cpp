9c084afe63ece1bd37347c90df36567058b485ac
fierval <fierval@gmail.com>
2021-02-24 23:26:03 -0800

    refactor main

--- networks/cifar/src/main.cpp
+++ networks/cifar/src/main.cpp
@@ -1,11 +1,4 @@
-#include "dataset.h"
-
-#include <losses/crossentropyloss.hpp>
-#include <device/device_tensor.hpp>
-#include <chrono>
-#include <vector>
-#include <string>
-#include "cifar10_network.hpp"
+#include "main.hpp"
 
 using namespace EigenSinn;
 using namespace Eigen;
@@ -75,49 +68,6 @@ int main(int argc, char* argv[]) {
   } 
   
   // Test
-  std::cout << "Starting test..." << std::endl;
-  DeviceTensor<float, 4> batch_tensor(create_batch_tensor(dataset.test_images, 0, dataset.test_images.size()));
-  Tensor<int, 1> label_tensor = create_1d_label_tensor(dataset.test_labels).cast<int>();
-
-  net.set_input(batch_tensor);
-  net.forward();
-
-  DeviceTensor<float, 2> test_output(net.get_output());
-
-  // extract predicted results from the output
-  DeviceTensor<Tuple<Index, float>, 2> test_index_tuples(test_output.dimensions());
-  test_index_tuples.view() = test_output->index_tuples();
-
-  DeviceTensor<Tuple<Index, float>, 1> pred_res(test_output.dimension(0));
-  pred_res.view() = test_index_tuples->reduce(array<Index, 1> {1}, internal::ArgMaxTupleReducer<Tuple<Index, float>>());
-  Tensor< Tuple<Index, float>, 1> h_pred_res = pred_res.to_host();
-
-  Tensor<int, 1> predictions(pred_res.dimension(0));
-
-  Tensor<float, 1> n_correct(num_classes), n_samples(num_classes), accuracy(num_classes);
-  n_correct.setZero();
-  n_samples.setZero();
-
-  // recover actual index value by unrolling the col-major stored index
-  for (Index i = 0; i < pred_res.dimension(0); i++) {
-    predictions(i) = (h_pred_res(i).first - i) / pred_res.dimension(0) % num_classes;
-    int label = label_tensor(i);
-    if (predictions(i) == label) {
-      n_correct(label)++;
-    }
-    n_samples(label)++;
-  }
-  
-  // overall accuracy
-  Tensor<float, 0> matches = (predictions == label_tensor).cast<float>().sum();
-  std::cout << "Accuracy: " << matches(0) / predictions.dimension(0) << std::endl;
-
-  // accuracy by class
-  accuracy = n_correct / n_samples;
-
-  for (int j = 0; j < num_classes; j++) {
-    std::cout << "Class: " << classes[j] << " Accuracy: " << accuracy(j) << std::endl;
-  }
-  
+  TestNetwork(dataset, net, num_classes, classes);
   return 0;
 }
---
+++ networks/cifar/src/main.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "dataset.h"
+
+#include <losses/crossentropyloss.hpp>
+#include <device/device_tensor.hpp>
+#include <chrono>
+#include <vector>
+#include <string>
+#include "cifar10_network.hpp"
+
+using namespace EigenSinn;
+
+template<typename Device_>
+inline void TestNetwork(cifar::CIFAR10_dataset<std::vector, Eigen::Tensor<float, 3>, uint8_t>& dataset, Cifar10<Device_>& net, int num_classes, std::vector<cv::String>& classes) {
+
+  std::cout << "Starting test..." << std::endl;
+  DeviceTensor<float, 4, Device_> batch_tensor(create_batch_tensor(dataset.test_images, 0, dataset.test_images.size()));
+  Tensor<int, 1> label_tensor = create_1d_label_tensor(dataset.test_labels).cast<int>();
+
+  net.set_input(batch_tensor);
+  net.forward();
+
+  DeviceTensor<float, 2, Device_> test_output(net.get_output());
+
+  // extract predicted results from the output
+  DeviceTensor<Tuple<Index, float>, 2, Device_> test_index_tuples(test_output.dimensions());
+  test_index_tuples.view() = test_output->index_tuples();
+
+  // flatten the output tensor and get the "winning" indices
+  DeviceTensor<Tuple<Index, float>, 1, Device_> pred_res(test_output.dimension(0));
+  pred_res.view() = test_index_tuples->reduce(array<Index, 1> {1}, internal::ArgMaxTupleReducer<Tuple<Index, float>>());
+  Tensor< Tuple<Index, float>, 1> h_pred_res = pred_res.to_host();
+
+  Tensor<int, 1> predictions(pred_res.dimension(0));
+
+  Tensor<float, 1> n_correct(num_classes), n_samples(num_classes), accuracy(num_classes);
+  n_correct.setZero();
+  n_samples.setZero();
+
+  // recover actual index value by unrolling the col-major stored index
+  for (Index i = 0; i < pred_res.dimension(0); i++) {
+    predictions(i) = (h_pred_res(i).first - i) / pred_res.dimension(0) % num_classes;
+    int label = label_tensor(i);
+    if (predictions(i) == label) {
+      n_correct(label)++;
+    }
+    n_samples(label)++;
+  }
+
+  // overall accuracy
+  Tensor<float, 0> matches = (predictions == label_tensor).cast<float>().sum();
+  std::cout << "Accuracy: " << matches(0) / predictions.dimension(0) << std::endl;
+
+  // accuracy by class
+  accuracy = n_correct / n_samples;
+
+  for (int j = 0; j < num_classes; j++) {
+    std::cout << "Class: " << classes[j] << " Accuracy: " << accuracy(j) << std::endl;
+  }
+
+}
