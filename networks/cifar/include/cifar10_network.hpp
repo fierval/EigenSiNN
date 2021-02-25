c7be1d941c911f33fd20bb41da8707ac32e11065
fierval <fierval@gmail.com>
2021-02-24 19:03:08 -0800

    refactor

--- networks/cifar/include/cifar10_network.hpp
+++ networks/cifar/include/cifar10_network.hpp
@@ -13,28 +13,28 @@ public:
     // push back rvalues so we don't have to invoke the copy constructor
     add(NetworkNode<float, Device_>(new Input<float, 4>));
 
-    network.push_back(NetworkNode<float, Device_>(new Conv2d<float>(array<Index, 4>{6, 3, 5, 5}, Padding2D{ 0, 0 }, 1), get_optimizer<4>(learning_rate)));
-    network.push_back(NetworkNode<float, Device_>(new ReLU<float, 4>));
-    network.push_back(NetworkNode<float, Device_>(new MaxPooling<float, 4>(array<Index, 2>{2, 2}, 2)));
-
-    network.push_back(NetworkNode<float, Device_>(new Conv2d<float>(array<Index, 4>{16, 6, 5, 5}, Padding2D{ 0, 0 }, 1), get_optimizer<4>(learning_rate)));
-    network.push_back(NetworkNode<float, Device_>(new ReLU<float, 4>));
-    network.push_back(NetworkNode<float, Device_>(new MaxPooling<float, 4>(array<Index, 2>{2, 2}, 2)));
+    add(NetworkNode<float, Device_>(new Conv2d<float>(array<Index, 4>{6, 3, 5, 5}, Padding2D{ 0, 0 }, 1), get_optimizer<4>(learning_rate)));
+    add(NetworkNode<float, Device_>(new ReLU<float, 4>));
+    add(NetworkNode<float, Device_>(new MaxPooling<float, 4>(array<Index, 2>{2, 2}, 2)));
+    
+    add(NetworkNode<float, Device_>(new Conv2d<float>(array<Index, 4>{16, 6, 5, 5}, Padding2D{ 0, 0 }, 1), get_optimizer<4>(learning_rate)));
+    add(NetworkNode<float, Device_>(new ReLU<float, 4>));
+    add(NetworkNode<float, Device_>(new MaxPooling<float, 4>(array<Index, 2>{2, 2}, 2)));
 
     // get flat dimension by pushing a zero tensor through the network defined so far
     int flat_dim = get_flat_dimension(array<Index, 4>{1, 3, 32, 32});
 
-    network.push_back(NetworkNode<float, Device_>(new Flatten<float>));
+    add(NetworkNode<float, Device_>(new Flatten<float>));
 
 
-    network.push_back(NetworkNode<float, Device_>(new Linear<float>(flat_dim, 120), get_optimizer<2>(learning_rate)));
-    network.push_back(NetworkNode<float, Device_>(new ReLU<float, 2>));
+    add(NetworkNode<float, Device_>(new Linear<float>(flat_dim, 120), get_optimizer<2>(learning_rate)));
+    add(NetworkNode<float, Device_>(new ReLU<float, 2>));
 
-    network.push_back(NetworkNode<float, Device_>(new Linear<float>(120, 84), get_optimizer<2>(learning_rate)));
-    network.push_back(NetworkNode<float, Device_>(new ReLU<float, 2>));
+    add(NetworkNode<float, Device_>(new Linear<float>(120, 84), get_optimizer<2>(learning_rate)));
+    add(NetworkNode<float, Device_>(new ReLU<float, 2>));
 
     // cross-entropy loss includes the softmax non-linearity
-    network.push_back(NetworkNode<float, Device_>(new Linear<float>(84, num_classes), get_optimizer<2>(learning_rate)));
+    add(NetworkNode<float, Device_>(new Linear<float>(84, num_classes), get_optimizer<2>(learning_rate)));
   }
 
 protected:
