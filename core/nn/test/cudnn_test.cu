aa3ba9a7bde4f27b9ed77e2d2bce6f11d799dace
fierval <fierval@gmail.com>
2021-03-18 19:27:17 -0700

    refactoring simple tests

--- core/nn/test/cudnn_test.cu
+++ core/nn/test/cudnn_test.cu
@@ -16,6 +16,9 @@ namespace EigenSinnTest {
     void SetUp() override {
       cd.init();
       cd1p.init();
+
+      params = std::make_shared<ConvolutionParams<4>>(cd.convInput.dimensions(), cd.convWeights.dimensions(), padding, stride, dilation, false);
+      W = std::make_shared<CudnnWorkspace>(device(), *params);
     }
 
     CommonData4d<CudnnDevice, RowMajor> cd;
@@ -24,6 +27,10 @@ namespace EigenSinnTest {
     const int stride = 1, dilation = 1;
     const Padding2D padding{ 0, 0 };
 
+    std::shared_ptr<ConvolutionParams<4> >params;
+    static inline DeviceWrapper<CudnnDevice> device;
+
+    std::shared_ptr<CudnnWorkspace> W;
   };
 
   TEST_F(CudnnTest, SimpleConvForward) {
@@ -33,20 +40,12 @@ namespace EigenSinnTest {
     // - input tensor
     // - filter
     // - convolution
-    cudnnTensorDescriptor_t input_desc;
-    cudnnTensorDescriptor_t output_desc;
-
-    ConvolutionParams<4> params(cd.convInput.dimensions(), cd.convWeights.dimensions(), padding, stride, dilation, false);
-    DeviceWrapper<CudnnDevice> device;
-
-    CudnnWorkspace W(device(), params);
-
-    DeviceTensor<float, 4, CudnnDevice, RowMajor> out(params.output_dims());
+    DeviceTensor<float, 4, CudnnDevice, RowMajor> out(params->output_dims());
 
     // forward convolution
-    checkCudnnErrors(cudnnConvolutionForward(W.cudnn(), &W.one, W.input_desc, cd.convInput->data(),
-      W.filter_desc, cd.convWeights->data(), W.conv_desc, W.conv_fwd_algo, W.d_workspace, W.workspace_size,
-      &W.zero, W.output_desc, out->data()));
+    checkCudnnErrors(cudnnConvolutionForward(W->cudnn(), &(W->one), W->input_desc, cd.convInput->data(),
+      W->filter_desc, cd.convWeights->data(), W->conv_desc, W->conv_fwd_algo, W->d_workspace, W->workspace_size,
+      &(W->zero), W->output_desc, out->data()));
 
     EXPECT_TRUE(is_elementwise_approx_eq(cd.output, out));
    
@@ -59,36 +58,29 @@ namespace EigenSinnTest {
     // - input tensor
     // - filter
     // - convolution
-    cudnnTensorDescriptor_t input_desc;
-    cudnnTensorDescriptor_t output_desc;
 
-    ConvolutionParams<4> params(cd.convInput.dimensions(), cd.convWeights.dimensions(), padding, stride, dilation, false);
-    DeviceWrapper<CudnnDevice> device;
-
-    CudnnWorkspace W(device(), params);
-
-    DeviceTensor<float, 4, CudnnDevice, RowMajor> out(params.output_dims());
-    DeviceTensor<float, 4, CudnnDevice, RowMajor> dinput(params.orig_dims());
-    DeviceTensor<float, 4, CudnnDevice, RowMajor> dweight(params.kernel_dims);
+    DeviceTensor<float, 4, CudnnDevice, RowMajor> out(params->output_dims());
+    DeviceTensor<float, 4, CudnnDevice, RowMajor> dinput(params->orig_dims());
+    DeviceTensor<float, 4, CudnnDevice, RowMajor> dweight(params->kernel_dims);
 
     // forward convolution
-    checkCudnnErrors(cudnnConvolutionForward(W.cudnn(), &W.one, W.input_desc, cd.convInput->data(),
-      W.filter_desc, cd.convWeights->data(), W.conv_desc, W.conv_fwd_algo, W.d_workspace, W.workspace_size,
-      &W.zero, W.output_desc, out->data()));
+    checkCudnnErrors(cudnnConvolutionForward(W->cudnn(), &(W->one), W->input_desc, cd.convInput->data(),
+      W->filter_desc, cd.convWeights->data(), W->conv_desc, W->conv_fwd_algo, W->d_workspace, W->workspace_size,
+      &(W->zero), W->output_desc, out->data()));
 
     EXPECT_TRUE(is_elementwise_approx_eq(cd.output, out));
 
     // data backwards
-    checkCudnnErrors(cudnnConvolutionBackwardData(W.cudnn(), &W.one, W.filter_desc, cd.convWeights->data(), 
-      W.output_desc, cd.convLoss->data(), W.conv_desc, W.conv_bwd_data_algo, W.d_workspace, W.workspace_size, &W.zero, W.input_desc, dinput->data()));
+    checkCudnnErrors(cudnnConvolutionBackwardData(W->cudnn(), &(W->one), W->filter_desc, cd.convWeights->data(), 
+      W->output_desc, cd.convLoss->data(), W->conv_desc, W->conv_bwd_data_algo, W->d_workspace, W->workspace_size, &(W->zero), W->input_desc, dinput->data()));
 
 
     EXPECT_TRUE(is_elementwise_approx_eq(cd.dinput, dinput));
 
     // weights backwards
     checkCudnnErrors(
-      cudnnConvolutionBackwardFilter(W.cudnn(), &W.one, W.input_desc, cd.convInput->data(), W.output_desc, cd.convLoss->data(),
-        W.conv_desc, W.conv_bwd_filter_algo, W.d_workspace, W.workspace_size, &W.zero, W.filter_desc, dweight->data()));
+      cudnnConvolutionBackwardFilter(W->cudnn(), &(W->one), W->input_desc, cd.convInput->data(), W->output_desc, cd.convLoss->data(),
+        W->conv_desc, W->conv_bwd_filter_algo, W->d_workspace, W->workspace_size, &(W->zero), W->filter_desc, dweight->data()));
 
     EXPECT_TRUE(is_elementwise_approx_eq(cd.dweight, dweight));
   }
