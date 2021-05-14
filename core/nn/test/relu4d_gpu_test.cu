6e3bcad5aa601aef7de5e4c942e9a02c476faa6a
Boris Kogan <fierval@gmail.com>
2021-05-14 09:15:51 -0700

    add relu4d test

---
+++ core/nn/test/relu4d_gpu_test.cu
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <layers/relu.hpp>
+#include "include/commondata2d.hpp"
+#include "include/testutils.hpp"
+#include "ops/comparisons.hpp"
+#include <layers/input.hpp>
+
+using namespace EigenSinn;
+
+namespace EigenSinnTest {
+
+  class ReLU4dGpu : public ::testing::Test { 
+  
+  };
+}
